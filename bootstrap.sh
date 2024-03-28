bb8b2f5001c9467294dfb24f0fd4f7666ec13b51
aramistm <victor.mommayev@gmail.com>
2024-03-28 22:54:55 +0500

    change env bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt install screen -y
 cd ~
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.85_beta/lolMiner_v1.85_beta_Lin64.tar.gz
 tar -xzvf lolMiner_v1.85_beta_Lin64.tar.gz
-echo "/root/1.85_beta/lolMiner --algo PYRIN --pool de.pyrin.herominers.com:1177 --user pyrin:qqag8k5fy8efvr22em0z054mcjksa77qr4p60ezxe90j5xmyrp4msct9pfxl6.$0" > /root/mine.sh
+echo "/root/1.85_beta/lolMiner --algo PYRIN --pool de.pyrin.herominers.com:1177 --user pyrin:qqag8k5fy8efvr22em0z054mcjksa77qr4p60ezxe90j5xmyrp4msct9pfxl6.$1" > /root/mine.sh
 screen -dmS mining bash /root/mine.sh
 echo "" >> /etc/supervisor/conf.d/supervisord.conf
 echo "[program:mining]" >> /etc/supervisor/conf.d/supervisord.conf
