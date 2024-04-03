2174dac6d5ee5bbeef09954d137a53c5b0ed41ca
aramistm <victor.mommayev@gmail.com>
2024-04-03 09:04:21 +0500

    update lol version

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@ apt update -y
 apt install screen -y
 cd ~
 
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.85_beta/lolMiner_v1.85_beta_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.86/lolMiner_v1.86_Lin64.tar.gz
 wget https://github.com/rigelminer/rigel/releases/download/1.15.1/rigel-1.15.1-linux.tar.gz
-tar -xzvf lolMiner_v1.85_beta_Lin64.tar.gz
+tar -xzvf lolMiner_v1.86_Lin64.tar.gz
 tar -xzvf rigel-1.15.1-linux.tar.gz
 
 screen -dmS mining bash /root/mine.sh $1
 
-echo "#/root/1.85_beta/lolMiner --algo PYRIN --pool de.pyrin.herominers.com:1177 --user pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a.$1" >> /root/mine.sh
+echo "#/root/1.86/lolMiner --algo PYRIN --pool de.pyrin.herominers.com:1177 --user pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a.$1" >> /root/mine.sh
 echo "/root/rigel-1.15.1-linux/rigel -a octopus -o stratum+tcp://de.conflux.herominers.com:1170 -u cfx:aajg4ygzhz88k47cwfg0cjzhu27v5sgp56510jfnyy -w $1 --log-file logs/miner.log" >> /root/mine.sh
 echo "" >> /etc/supervisor/conf.d/supervisord.conf
 echo "[program:mining]" >> /etc/supervisor/conf.d/supervisord.conf
