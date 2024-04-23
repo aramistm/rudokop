1575f36a3a63407ed9535ed100cb05a6809b87bf
aramistm <victor.mommayev@gmail.com>
2024-04-23 10:14:12 +0500

    rigel 1.62, PYI default

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,17 @@ apt install screen nano -y
 cd ~
 
 wget http://178.208.78.132/lolMiner_v1.88_Lin64.tar.gz
-wget http://178.208.78.132/rigel-1.15.1-linux.tar.gz
+wget http://178.208.78.132/rigel-1.16.2-linux.tar.gz
 tar -xzvf lolMiner_v1.88_Lin64.tar.gz
-tar -xzvf rigel-1.15.1-linux.tar.gz
+tar -xzvf rigel-1.16.2-linux.tar.gz
 
 screen -dmS mining bash /root/mine.sh $1
 
 echo "#/root/1.88/lolMiner --algo PYRIN --pool de.pyrin.herominers.com:1177 --user pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a.$1" >> /root/mine.sh
-echo "#/root/rigel-1.15.1-linux/rigel -a octopus -o stratum+tcp://de.conflux.herominers.com:1170 -u cfx:aajg4ygzhz88k47cwfg0cjzhu27v5sgp56510jfnyy -w $1 --log-file logs/miner.log" >> /root/mine.sh
-echo "#/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode PYRINDUAL --dualpool de.pyrin.herominers.com:1177 --dualuser pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a.$1" >> /root/mine.sh
+echo "#/root/rigel-1.16.2-linux/rigel -a octopus -o stratum+tcp://de.conflux.herominers.com:1170 -u cfx:aajg4ygzhz88k47cwfg0cjzhu27v5sgp56510jfnyy -w $1 --log-file logs/miner.log" >> /root/mine.sh
+echo "/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode PYRINDUAL --dualpool de.pyrin.herominers.com:1177 --dualuser pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a.$1" >> /root/mine.sh
 echo "#/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode TONDUAL --dualpool ton.hashrate.to:4002 --dualuser UQDPEUpB98MhV0LMIPGTix5zavzuwrcR5wkrNcHuhDG0LEE3.$1" >> /root/mine.sh
-echo "/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode TONDUAL --dualpool stratum+tcp://stratum.hyperpool.pro:8040 --ton-mode 6 --dualuser eaf89db7108470dc3f6b23eac30358114f56f89c99b0f98913256c3ccf752a3bf46bab7457
+echo "#/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode TONDUAL --dualpool stratum+tcp://stratum.hyperpool.pro:8040 --ton-mode 6 --dualuser eaf89db7108470dc3f6b23eac30358114f56f89c99b0f98913256c3ccf752a3bf46bab7457
 " >> /root/mine.sh
 
 
