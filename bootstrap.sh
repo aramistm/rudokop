3ffd6bc5804ef50e9ffafd26c0666591af7c3d44
aramistm <victor.mommayev@gmail.com>
2024-05-18 11:31:10 +0500

    rigel update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,13 @@ apt install -y g++-11 screen nano libc6 htop
 cd ~
 
 wget http://178.208.78.132/lolMiner_v1.88_Lin64.tar.gz
-wget http://178.208.78.132/rigel-1.17.1-linux.tar.gz
+wget http://178.208.78.132/rigel-1.17.2-linux.tar.gz
 wget http://178.208.78.132/qli-Client-1.9.6-Linux-x64.tar.gz
 
 
 tar -xzvf qli-Client-1.9.6-Linux-x64.tar.gz
 tar -xzvf lolMiner_v1.88_Lin64.tar.gz
-tar -xzvf rigel-1.17.1-linux.tar.gz
+tar -xzvf rigel-1.17.2-linux.tar.gz
 
 mkdir qcpu
 cp qli-Client /root/qcpu/
@@ -27,11 +27,11 @@ echo "#/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --
 echo "#/root/1.88/lolMiner -a FISHHASH --pool de.ironfish.herominers.com:1145 --user c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458.$1 --dualmode TONDUAL --dualpool stratum+tcp://stratum.hyperpool.pro:8040 --ton-mode 6 --dualuser eaf89db7108470dc3f6b23eac30358114f56f89c99b0f98913256c3ccf752a3bf46bab7457" >> /root/mine.sh
 
 echo "#Rigel" >> /root/mine.sh
-echo "#/root/rigel-1.17.1-linux/rigel -a fishhash+sha256ton+zil -o [1]stratum+tcp://de.ironfish.herominers.com:1145 -u [1]c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458 -o [2]stratum+tcp://stratum.hyperpool.pro:8040 -u [2]eaf89db7108470dc3f6b23eac30358114f56f89c99b0f98913256c3ccf752a3bf46bab7457 -o [3]stratum+tcp://eu.zil.k1pool.com:1111 -u [3]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -w $1 --log-file logs/miner.log" >> /root/mine.sh
-echo "#/root/rigel-1.17.1-linux/rigel -a fishhash+pyrinhash+zil -o [1]stratum+tcp://de.ironfish.herominers.com:1145 -u [1]c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458 -o [2]stratum+tcp://de.pyrin.herominers.com:1177 -u [2]pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a -o [3]stratum+tcp://eu.zil.k1pool.com:1111 -u [3]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -w $1 --log-file logs/miner.log" >> /root/mine.sh
+echo "#/root/rigel-1.17.2-linux/rigel -a fishhash+sha256ton+zil -o [1]stratum+tcp://de.ironfish.herominers.com:1145 -u [1]c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458 -o [2]stratum+tcp://stratum.hyperpool.pro:8040 -u [2]eaf89db7108470dc3f6b23eac30358114f56f89c99b0f98913256c3ccf752a3bf46bab7457 -o [3]stratum+tcp://eu.zil.k1pool.com:1111 -u [3]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -w $1 --log-file logs/miner.log" >> /root/mine.sh
+echo "#/root/rigel-1.17.2-linux/rigel -a fishhash+pyrinhash+zil -o [1]stratum+tcp://de.ironfish.herominers.com:1145 -u [1]c9f8d6c1849abbcd164f6c72002d9ac44b9deaef70481739a29d1733915defca+107458 -o [2]stratum+tcp://de.pyrin.herominers.com:1177 -u [2]pyrin:qq4mqzjm7uradurlg98nx7mwhak9y2dflh8zeuer6pe65sx4c9prqqrwelk0a -o [3]stratum+tcp://eu.zil.k1pool.com:1111 -u [3]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -w $1 --log-file logs/miner.log" >> /root/mine.sh
 
 echo "#xelis" >> /root/mine.sh
-echo "/root/rigel-1.17.1-linux/rigel -a xelishash+zil -o [1]stratum+ssl://eu.xel.k1pool.com:9352 -u [1]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -o [2]stratum+tcp://eu.zil.k1pool.com:1111 -u [2]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn --zil-countdown -w $1" >> /root/mine.sh
+echo "/root/rigel-1.17.2-linux/rigel -a xelishash+zil -o [1]stratum+ssl://eu.xel.k1pool.com:9352 -u [1]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn -o [2]stratum+tcp://eu.zil.k1pool.com:1111 -u [2]KrHmLGJQ4fidmeS9Hn9khDHWkUddAi1L4Vn --zil-countdown -w $1" >> /root/mine.sh
 
 echo "#Qubic" >> /root/mine.sh
 echo "#screen -dmS qubic ./qli-Client" >> /root/mine.sh
--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Скачиваем архив с программой Rigel
-wget https://github.com/rigelminer/rigel/releases/download/1.17.1/rigel-1.17.1-linux.tar.gz
+wget https://github.com/rigelminer/rigel/releases/download/1.17.3/rigel-1.17.3-linux.tar.gz
 
 # Распаковываем архив
-tar -zxf rigel-1.17.1-linux.tar.gz
+tar -zxf rigel-1.17.3-linux.tar.gz
 
 # Закрываем screen с именем "mining"
 screen -S mining -X quit
 
 # Заменяем версию в скрипте mine.sh
-sed -i 's/rigel-1.17.0/rigel-1.17.1/g' mine.sh
+sed -i 's/rigel-1.17.0/rigel-1.17.3/g' mine.sh
 
 # Запускаем скрипт mine.sh в новом screen с именем "mining"
 screen -dmS mining bash /root/mine.sh
