259f13af0a72fc2df77ba8be9fdb647b41132534
aramistm <victor.mommayev@gmail.com>
2024-09-13 19:01:40 +0500

    fix app

--- bootstrap.sh
+++ bootstrap.sh
@@ -58,9 +58,7 @@ echo "{
     \"accessToken\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImFmZTQ5ZGZkLWM2YzUtNDhiNi05NDllLTVhODkyNmIyMzY4ZSIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNjIzNDUwNCwiZXhwIjoxNzU3NzcwNTA0LCJpYXQiOjE3MjYyMzQ1MDQsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.SIgFxJFbGr11OGtp85Kn8RZdxcfIWTAh6YSmf1unQLBbW9gvB659Rfs0juF9MsFNVaUX9KF-E3qesaU0qOEDx4s5fDekkNSM5BST2QDOdAR3qgojhuktAlB24gnwv7HPim-Nlsna1jT7xkT3qqDRa4n0ZT1lnooRXgh70prQusY37-xA72-DXY78f_XvaX50AIfit2Od8XCTERAkHi8h56H3WjkwqUiOvU7QH03jwyevuuJu4jEUPOlI76iAw6fl-w2nsZjK1F2HHZSGHIeqsMFpLdBX_84fstkNp6BozXKL2sJ1eOb_7T2UxC2_16C3HPw9nuL9GgXAyX_Q59zFPA\",
     \"idleSettings\": true
   }
-}" > appsettings.json
-
-echo "{\"Settings\":{\"baseUrl\": \"https://mine.qubic.li/\",\"amountOfThreads\": 24,\"alias\": \"$1_CPU\",\"accessToken\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImFmZTQ5ZGZkLWM2YzUtNDhiNi05NDllLTVhODkyNmIyMzY4ZSIsIk1pbmluZyI6IiIsIm5iZiI6MTcyNDMzMjgyMCwiZXhwIjoxNzU1ODY4ODIwLCJpYXQiOjE3MjQzMzI4MjAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.eu8nMWTWVJfywr45W46bHbMABpFakTPRgmjQUezTIlIH8SEvyRKf_9IQv_P9TQPtZsyUJOvIL1L23gCXvTiI87V_XNEamFY1_HqftkRVkjuPKgjPlVcmk1u4ChUAJp-tIMvsuTJ9QBj7MA2VAv0rfpXSWHNolXPQiwEV-8uWIMmQNhgLEWMYbhz0tVVd1-fqHg3UK4vCtkC7RFoIoD3nuU5Ey9LABp7wJZOxcEMe21JwtEPyW0Q9lzQBtcYhumV2EjFankYIjoBOeDMtI-8u-xx7ywA2OVHEVUkPGhAp_3tTRMy1G-VXMDJsESOxhVil9TkY7i1KpH_ePSCNBhOH-Q\", \"autoupdateEnabled\": true}}" > /root/qcpu/appsettings.json
+}" > /root/qcpu/appsettings.json
 
 
 echo "#Monitoring" >> /root/mine.sh
